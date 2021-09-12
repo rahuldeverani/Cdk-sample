21b5232ef11440e98f3fdf75e25bca72612ce425
rahuldeverani <rahuldeverani@gmail.com>
2021-09-12 12:20:22 +0530

    Add Lambda inline sample

--- lib/helloWorldLambda.ts
+++ lib/helloWorldLambda.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class helloworld extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,6 +11,15 @@ export class helloworld extends cdk.Stack {
             runtime: lambda.Runtime.NODEJS_14_X,
             handler: 'hello-world.handler',
             code: lambda.Code.fromAsset('lambda')
+
+        });
+
+
+
+        const func2 = new lambda.Function(this, "myFunc2", {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'index.handler',
+            code: lambda.Code.fromInline(fs.readFileSync('lambda/hello-world.js', { encoding: 'utf-8' }))
 
         });
 
